4bc2122d87a23f8461c0a863ee32e9e759af2eaf
rk696693-byte <rk696693@gmail.com>
2026-07-06 23:18:38 +0530

    update code

---
+++ .vscode/challenges.py/New folder/11pj.py
@@ -0,0 +1,7 @@
+#while looop
+n=int(input("enter your number"))
+sum=0
+i=1
+while i<n:
+    sum=sum+i
+print(sum)
---
+++ .vscode/challenges.py/New folder/sixthpj.py
@@ -0,0 +1,9 @@
+#operator 2
+a=input("enter a character")
+if a>=a and a<=z:
+    print("its a character")
+elif A>=A and A<+Z:
+    print("its a character")
+else:
+    print("its not a character")
+    
