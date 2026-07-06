f59c1a52c462eea036504ef5253900c08a46efd0
rk696693-byte <rk696693@gmail.com>
2026-07-06 23:33:39 +0530

    update code

---
+++ .vscode/challenges.py/New folder/seventhpj.py
@@ -0,0 +1,11 @@
+ #OPERATOR 3
+ a=input("enter a value")
+ if a=1:
+    print("its a number")
+if number>+50 and number<=100:
+    print("its a digits")
+elif number>+30 and number<=60:
+    print("its a digits") 
+else:
+    print("its not a digits")                                             
+  
