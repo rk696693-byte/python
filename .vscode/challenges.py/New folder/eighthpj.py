39708eb5b073c68ff763da6da1458f8bba023151
rk696693-byte <rk696693@gmail.com>
2026-07-06 23:47:54 +0530

    update code

---
+++ .vscode/challenges.py/New folder/eighthpj.py
@@ -0,0 +1,10 @@
+#number swap
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+print("before swapping",a,b,c)
+x=a
+a=b
+b=c
+c=x
+print("after swapping",a,b,c)
